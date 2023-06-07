1647f1f7c2728a7ead6789510c3e9155fa0e1ebd
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-06-07 07:38:49 +0100

    Update searchData.js

--- routes/searchData.js
+++ routes/searchData.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const searchDataCtrl = require('../controllers/searchData');
 
-router.post('/save', searchDataCtrl.saveTradeRequest);
 router.get('/search', searchDataCtrl.searchData); // Ajoutez cette ligne pour la nouvelle route de recherche
 
 module.exports = router;
