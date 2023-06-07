a442c9302a7740298e920b3f4d6955cd7e0111f0
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-06-07 07:43:23 +0100

    Update searchData.js

--- controllers/searchData.js
+++ controllers/searchData.js
@@ -1,14 +1,16 @@
-exports.searchData = (req, res, next) => {
+exports.searchData = async (req, res, next) => {
   const { option } = req.query;
 
-  // Effectuez votre logique de recherche avec la valeur de l'option
-  // Utilisez la méthode appropriée de votre modèle Mongoose pour interroger la base de données
+  try {
+    const db = req.app.locals.db; // Utilisez l'objet db existant provenant de la connexion à la base de données
 
-  TradeRequest.find({ date: option }) // Exemple de recherche sur la propriété "date" égale à l'option sélectionnée
-    .then((results) => {
-      res.status(200).json(results);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Erreur lors de la recherche dans la base de données' });
-    });
+    const collection = db.collection('nom_de_votre_collection'); // Remplacez par le nom réel de votre collection
+
+    const results = await collection.find({ date: option }).toArray();
+
+    res.status(200).json(results);
+  } catch (error) {
+    console.log('Erreur lors de la recherche dans la base de données :', error);
+    res.status(500).json({ error: 'Erreur lors de la recherche dans la base de données' });
+  }
 };
