5a2603daeec9c8b4bebb086dae71610c5b4fb848
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-06-11 15:48:06 +0100

    R

--- controllers/searchData.js
+++ controllers/searchData.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { exec } = require('child_process');
 
 exports.searchData = async (req, res, next) => {
@@ -28,19 +27,6 @@ exports.searchData = async (req, res, next) => {
       // Envoyer les résultats à votre front-end si nécessaire
       res.status(200).json({ results });
     });
-=======
-exports.searchData = async (req, res, next) => {
-  const { option } = req.query;
-
-  try {
-    const db = req.app.locals.db; // Utilisez l'objet db existant provenant de la connexion à la base de données
-
-    const collection = db.collection('nom_de_votre_collection'); // Remplacez par le nom réel de votre collection
-
-    const results = await collection.find({ date: option }).toArray();
-
-    res.status(200).json(results);
->>>>>>> 09aa3bd86aed092e01edcc6560781a9ce47b3273
   } catch (error) {
     console.log('Erreur lors de la recherche dans la base de données :', error);
     res.status(500).json({ error: 'Erreur lors de la recherche dans la base de données' });
